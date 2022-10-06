e598b822ebcdea49b6bf8ca8edb46de93c265615
Sean Sáez Fuller <sean.saez.f@gmail.com>
2022-10-06 14:39:39 +0200

    Seccion 14 finalizada - Optimizacion de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Title from './components/Title'
+import MyForm from './components/Forms/MyForm'
+import MyList from './components/Lists/MyList'
 
 function App() {
+  const [valores, setValores] = useState([])
+  const handleSubmit = (values) => {
+    setValores([
+      ...valores,
+      values
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>Mi título</Title>
+      <MyForm onSubmit={handleSubmit} />
+      <MyList data={valores} />
     </div>
   );
 }
